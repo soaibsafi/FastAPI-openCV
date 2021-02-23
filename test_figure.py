157da7eb75159343d179d2cff9081d108260505d
soaibsafi <soaib.safi@gmail.com>
2021-02-23 22:39:39 +0100

    :tada:Project setup

---
+++ test_figure.py
@@ -0,0 +1,80 @@
+import pandas as pd
+import plotly.graph_objs as go
+
+
+def return_figures():
+    """Creates four plotly visualizations
+
+    Args:
+        None
+
+    Returns:
+        list (dict): list containing the four plotly visualizations
+
+    """
+
+    graph_one = []
+    graph_one.append(
+        go.Scatter(
+            x=[0, 1, 2, 3, 4, 5],
+            y=[0, 2, 4, 6, 8, 10],
+            mode='lines'
+        )
+    )
+
+    layout_one = dict(title='Chart One',
+                      xaxis=dict(title='x-axis label'),
+                      yaxis=dict(title='y-axis label'),
+                      )
+
+    graph_two = []
+
+    graph_two.append(
+        go.Bar(
+            x=['a', 'b', 'c', 'd', 'e'],
+            y=[12, 9, 7, 5, 1],
+        )
+    )
+
+    layout_two = dict(title='Chart Two',
+                      xaxis=dict(title='x-axis label', ),
+                      yaxis=dict(title='y-axis label'),
+                      )
+
+    graph_three = []
+    graph_three.append(
+        go.Scatter(
+            x=[5, 4, 3, 2, 1, 0],
+            y=[0, 2, 4, 6, 8, 10],
+            mode='lines'
+        )
+    )
+
+    layout_three = dict(title='Chart Three',
+                        xaxis=dict(title='x-axis label'),
+                        yaxis=dict(title='y-axis label')
+                        )
+
+    graph_four = []
+
+    graph_four.append(
+        go.Scatter(
+            x=[20, 40, 60, 80],
+            y=[10, 20, 30, 40],
+            mode='markers'
+        )
+    )
+
+    layout_four = dict(title='Chart Four',
+                       xaxis=dict(title='x-axis label'),
+                       yaxis=dict(title='y-axis label'),
+                       )
+
+    # append all charts to the figures list
+    figures = []
+    figures.append(dict(data=graph_one, layout=layout_one))
+    figures.append(dict(data=graph_two, layout=layout_two))
+    figures.append(dict(data=graph_three, layout=layout_three))
+    figures.append(dict(data=graph_four, layout=layout_four))
+
+    return figures
